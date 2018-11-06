3de66b39f98f3a88a5cc06b62f0826854b15a3a4
aspiringguru <bmatthewtaylor@gmail.com>
2018-11-06 07:57:07 +0000

    webhook added, load password from external file added

--- automationservices_ga_server.js
+++ automationservices_ga_server.js
@@ -1,9 +1,47 @@
-#!/usr/bin/env nodejs
-'use strict';
+'use strict'
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const app = express();
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.listen(4000, () => console.log('Webhook server is listening, port 4000'));
+const express = require('express')
+const bodyParser = require('body-parser')
+const request = require('request')
+
+const app = express()
+
+app.set('port', (4000));
+
+const fs = require('fs');
+const contents = fs.readFileSync('automationservices_ga_passwords.json');
+var jsonContent = JSON.parse(contents);
+console.log("verifyToken:", jsonContent.verifyToken);
+
+// Allows us to process the data
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+// ROUTES
+
+app.get('/', function(req, res) {
+	res.send("Hi I am a chatbot running on automationservices.ga")
+})
+
+app.get('/blah', function(req, res) {
+	res.send("This is blah. running on automationservices.ga")
+})
+
+let token = ""
+
+// routes for Facebook
+
+app.get('/webhook/', function(req, res) {
+  console.log("app.get('/webhook/' start");
+	if (req.query['hub.verify_token'] === jsonContent.verifyToken) {
+    console.log("hub.verify_token equals jsonContent.verifyToken");
+		res.send(req.query['hub.challenge'])
+	}
+	res.send("Wrong token")
+  console.log("hub.verify_token does not match jsonContent.verifyToken");
+})
+
+
+app.listen(app.get('port'), function() {
+	console.log("running, listening....")
+})
